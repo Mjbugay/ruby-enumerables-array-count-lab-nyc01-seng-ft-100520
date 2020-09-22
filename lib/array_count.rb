b84553586db516a6d60b8386b5714722ebeb4575
Matt Bugay <mjbugay@yahoo.com>
2020-09-22 01:22:57 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,10 +1,12 @@
 def count_strings(array)
   array.count do |index|
     array[index].class == String
+  end
 end
 
 def count_empty_strings(array)
   array.count do |index|
     array[index] == " "
+  end
 end
 
